f26b8027427db9b83157bdf959b24d654aac036d
U-DS\xpb14158 <xpb14158@CIS-KELVIN-07.cis.strath.ac.uk>
2017-10-02 16:47:52 +0100

    final changes to uml

--- src/Parser.java
+++ src/Parser.java
@@ -1,4 +1,24 @@
 
 public class Parser {
-
+public void setFile() {
+      // TODO implement this operation
+      throw new UnsupportedOperationException("not implemented");
+   }
+   
+   public void parseLine() {
+      // TODO implement this operation
+      throw new UnsupportedOperationException("not implemented");
+   }
+   
+   public Station parseStation() {
+      // TODO implement this operation
+      throw new UnsupportedOperationException("not implemented");
+   }
+   
+   public Route parseRoute() {
+      // TODO implement this operation
+      throw new UnsupportedOperationException("not implemented");
+   }
+   
+   
 }
