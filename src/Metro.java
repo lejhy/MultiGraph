24a2cbb9dc7680108e56113c0b3abf69511c0399
Daniela Filipova <wlb15162@strath.ac.uk>
2017-10-04 16:12:14 +0100

    changes to Metro

--- src/Metro.java
+++ src/Metro.java
@@ -1,13 +1,12 @@
 public class Metro {
-	
+
 	private MultiGraph<Station, Route<Station>> graph;
-	
-	
+
 	public Metro() {
-		//create multiGraph
+		// create multiGraph
 		graph = new AdjacencyMapMultiGraph<>();
 	}
-	
+
 	public void populate(String fileName) {
 		Parser parser = new Parser(fileName);
 		for (Station station : parser.getStations()) {
@@ -17,18 +16,33 @@ public class Metro {
 			graph.addEdge(route);
 		}
 	}
-	
+
 	public String findPath(String startName, String endName) {
-		//Takes in the name of two stations and returns the fastest route between them in 
-		//the form of a list of individual routes
+		// Takes in the name of two stations and returns the fastest route
+		// between them in
+		// the form of a list of individual routes
 		Station start = getStation(startName);
 		Station end = getStation(endName);
 		if (start != null && end != null) {
 			String path = "";
 			path.concat(startName);
+			Route<Station> lastRoute = null;
+			String lastRouteLabel = "";
 			for (Route<Station> route : graph.getPath(start, end)) {
-				path.concat(route.getLabel());
-				//TODO needs some work
+				if (route.getLabel().equals(lastRouteLabel) != true) {
+					path.concat(lastRouteLabel);
+					lastRouteLabel = route.getLabel();
+					
+					if (lastRoute.getNodeIn().equals(route.getNodeOut())) {
+						path.concat(route.getNodeOut().getName());
+					}
+					
+					if (lastRoute.getNodeOut().equals(route.getNodeIn())) {
+						path.concat(route.getNodeIn().getName());
+					}
+				}
+				
+				lastRoute = route;
 			}
 			path.concat(endName);
 			return path;
@@ -37,14 +51,13 @@ public class Metro {
 		}
 	}
 
-	public Station getStation (String name) {
-		//Takes the name of a station and returns it as a Sation type
+	public Station getStation(String name) {
+		// Takes the name of a station and returns it as a Station type
 		for (Station station : graph.getNodes()) {
 			if (station.getName().equals(name)) {
 				return station;
 			}
-		}		
+		}
 		return null;
-	} 
+	}
 }
-
