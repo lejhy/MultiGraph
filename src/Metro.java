2550a8c6692991faf54026792e9150dc3a2b3550
Heather McLean <szb15138@uni.strath.ac.uk>
2017-10-05 11:43:30 +0100

    modified output

--- src/Metro.java
+++ src/Metro.java
@@ -28,13 +28,14 @@ public class Metro {
 		Station start = getStation(choiceStart);
 		Station end = getStation(choiceEnd);
 		if (start != null && end != null) {
-			String path = startName;
+			String path = "Board Metro at ";
+			path = path.concat(startName);
+			path = path.concat(" on the ");
 			Route<Station> lastRoute = null;
 			for (Route<Station> route : graph.getPath(start, end)) {
 				if (lastRoute == null) {
-					path = path.concat(" where you board ");
 					path = path.concat(route.getLabel());
-					path = path.concat(" and set off towards ");
+					path = path.concat(" Line\nFollow this line to ");
 				} else if (route.getLabel().equals(lastRoute.getLabel()) == false) {
 					
 					if (lastRoute.getNodeIn().equals(route.getNodeOut())) {
@@ -53,9 +54,9 @@ public class Metro {
 						path = path.concat(route.getNodeOut().getName());
 					}
 					
-					path = path.concat(" where you switch to ");
+					path = path.concat(" where you switch to the ");
 					path = path.concat(route.getLabel());
-					path = path.concat(" and continue towards ");
+					path = path.concat(" line\nFollow this line to ");
 				}
 				lastRoute = route;
 			}
